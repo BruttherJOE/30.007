9e9777d360cb03e94b05a366e914a1517645ee2d
bruttherjoe <bruttherjoe@gmail.com>
2021-03-30 15:52:42 +0800

    added code to control pipette stepper

---
+++ test/pipette_actuator.py
@@ -0,0 +1,33 @@
+import stepper_param_RPI as steppy
+
+# pipette works by pressing button down to certain height, 
+# then it only draws liquid when the button is depressed.
+
+# MAIN FUNCTION HERE
+def draw_liquid(vol):
+    press_pipette_button(vol)
+    depress_pipette_button(vol)
+
+# MOVEMENT FUNCTIONS HERE
+def press_pipette_button(vol): #vol is in terms of degrees stepped by stepper motor
+    c = 1 # constant for ppl to tune (degrees to ml) if we are not lazy
+    print('pressing on pipette button...')
+    steppy.engageStepper()
+    time.sleep(0.5)
+    steppy.rotate(c*vol,False,1)
+    time.sleep(0.1)
+    steppy.releaseStepper()
+
+def depress_pipette_button(vol):
+    c = 1
+    print('depressing the pipette button...')
+    steppy.engageStepper()
+    time.sleep(0.5)
+    steppy.rotate(c*vol,True,1)
+    time.sleep(0.1)
+    steppy.releaseStepper()
+
+## need callibrate with limit switch? or just screw it - whatver works works ¯\_(ツ)_/¯
+
+if __name__ == "__main__":
+    draw_liquid(720) # just edit this number for degrees turned by stepper
---
+++ test/stepper_param_RPI.py
@@ -0,0 +1,60 @@
+import RPi.GPIO as GPIO
+import time
+
+enaPin = 16
+dirPin = 20
+pulPin = 21
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(enaPin, GPIO.OUT)
+GPIO.setup(dirPin, GPIO.OUT)
+GPIO.setup(pulPin, GPIO.OUT)
+
+PULSE_PER_REV = 200
+
+def step(totalPulseTime): #in terms of seconds
+    GPIO.output(pulPin,GPIO.HIGH)
+    time.sleep(totalPulseTime/2)
+    GPIO.output(pulPin,GPIO.LOW)
+    time.sleep(totalPulseTime/2)
+
+def rotate(degrees, isClockwise, rps=1): #all values multiple of 360/PULSE_PER_REV
+    pulseCount = int(degrees/360.0 * PULSE_PER_REV)
+    pulseRate = PULSE_PER_REV * rps
+    rotatePulses(pulseCount, isClockwise, pulseRate)
+
+def rotatePulses(pulseCount, isClockwise, pulseRate): #pulseRate in pulses/sec
+    if isClockwise:
+        GPIO.output(dirPin,GPIO.LOW)
+    else:
+        GPIO.output(dirPin,GPIO.HIGH)
+    engageStepper()
+    for i in range(pulseCount):
+        step(1/(pulseRate))
+    releaseStepper()
+
+    GPIO.output(dirPin,GPIO.LOW)
+
+def releaseStepper():
+    GPIO.output(enaPin,GPIO.HIGH)
+
+def engageStepper():
+    GPIO.output(enaPin,GPIO.LOW)
+
+
+
+####### TEST ########
+
+#while True:
+#    engageStepper()
+#    time.sleep(2)
+#
+#    rotate(360, False, 1) #deg, isClockwise, rps
+#    time.sleep(0.1)
+#    rotate(360, True, 2)
+#    time.sleep(0.1)
+#    
+#
+#    rotate(180, False, 5)
+#    time.sleep(0.1)
+#    time.sleep(5)
